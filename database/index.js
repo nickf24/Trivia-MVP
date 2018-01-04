398f7cfed6cd4fa4e642dd4b3a20a34f2a7e4d3f
Nick Forster <nforster24@gmail.com>
2018-01-04 12:10:22 -0800

    functioning post to DB

--- database/index.js
+++ database/index.js
@@ -1,21 +1,48 @@
 let mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/trivia', { useMongoClient: true })
+const triviaAPI = require('../lib/triviaAPI.js')
+mongoose.connect('mongodb://localhost/trivia');
 
-let questionSchema = mongoose.Schema({
-  question: string,
-  answer: string,
-  category: string,
-  difficulty: string,
+let db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MONGO connection error!'));
+db.once('open', function() { 
+
+	console.log('connected to DB!');
+
 });
 
-let Questions = mongoose.model('Questions', questionSchema);
 
-let save = function(arr) {
-	// inserts data to DB
+
+let questionSchema = mongoose.Schema({
+  category: String,
+  type: String,
+  difficulty: String,
+  question: String,
+  correct_answer: String,
+  incorrect_answers: Array
+});
+
+
+let Question = mongoose.model('Questions', questionSchema);
+
+let save = function(params, results) {
+
+	for (var i = 0; i < results.length; i++) {
+		console.log(results[i].incorrect_answer)
+  	  	var questionInst = new Question({category: results[i].category, type: results[i].type, difficulty: results[i].difficulty, 
+  	  		question: results[i].question, correct_answer: results[i].correct_answer, incorrect_answers: results[i].incorrect_answers});
+  	
+  	    questionInst.save(function(err) {
+  	      if (err) {
+  	      	return handleError(err);
+  	      }
+  	    });
+  	}
 }
 
 let getQuestions = function() {
 	// fetches questions from DB
+
 }
 
 module.exports.save = save;
--- server/index.js
+++ server/index.js
@@ -3,18 +3,31 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const triviaAPI = require('../lib/triviaAPI.js');
+const db = require('../database/index.js');
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/questions', (req, res) => {
   // get questions from DB, serves to client
-  var params = {};
-  triviaAPI.getQuestions()
+  
+  res.send('hello world');
 });
 
 app.post('/questions', (req, res) => {
   // post questions to DB from client
+  // sends questions to DB with params
+  // triggered on category change
+  var params = {amount: 10, category: '23', difficulty: 'medium'};
+
+  triviaAPI.getQuestions(params, function(err, results) {
+  	if (err) {
+  	  console.log(err);
+  	} else {
+  	  db.save(params, results.data.results);
+  	  res.send('Successfully saved to the database!');
+  	}
+  })
 });
 
 
